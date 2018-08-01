7fa17bb8cca5a5458456faac6cea46fcc3cb1e84
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-08-01 16:23:27 +0300

    react

---
+++ Day 02 - 01.08.2018/05_props/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+import App from './App';
+
+ReactDOM
+.render(<App title="Best Pizza in the world" msg="Welcome" />,
+    document.getElementById('root'));
+   
+registerServiceWorker();
